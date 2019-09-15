45eca0d5a19c11760a57fe7792e29058c4473a37
Natalie-Cardot <nataliecardot@gmail.com>
2019-09-14 22:53:24 -0700

    Add functionality for goal deletion

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div class="container-fluid">
     <!-- Could pass as "newGoal($event)" but no need; Vue.js does it implicitly -->
     <app-new-goal @goalAdded="newGoal"></app-new-goal>
-    <app-goal-grid :goals="goals"></app-goal-grid>
+    <!-- Not relevant that deleteGoal is used in both GoalGrid and this root component; naming could be different. In this file it refers to the method defined below. Also, index is automatically passed, as content was automatically passed for @goalAdded above -->
+    <app-goal-grid :goals="goals" @goalDeleted="deleteGoal"></app-goal-grid>
     <div class="row">
       <div class="col text-center">
         <div class="alert alert-info">Note: Click on a goal to delete it.</div>
@@ -29,6 +30,10 @@
       // Emit event from NewGoal.vue automatically passed to parent (this file); using it here
       newGoal(goal) {
         this.goals.push(goal);
+      },
+      deleteGoal(index) {
+        // Modifies existing array, removing 1 item from it at the specified index
+        this.goals.splice(index, 1);
       }
     },
     components: {
--- src/components/GoalGrid.vue
+++ src/components/GoalGrid.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="row justify-content-center">
-    <app-goal v-for="(goal, index) in goals" v-bind:key="index">{{ goal }}</app-goal>
+    <!-- Native modifier instructs Vue.js to react to a click on that component if it happens on the native element of this component. So it registers the click on the app-goal component div, which is what sits in the HTML code in the end, with the native modifier, Vue.js knows that even though the click happened on the HTML code of that component, it should treat it as if it were a click on app-quote (not possible without modifier since it's not appended to the DOM) -->
+    <app-goal v-for="(goal, index) in goals" v-bind:key="index" @click.native="deleteGoal(index)">{{ goal }}</app-goal>
   </div>
 </template>
 
@@ -11,6 +12,11 @@
     props: ['goals'],
     components: {
       appGoal: Goal
+    },
+    methods: {
+      deleteGoal(index) {
+        this.$emit('goalDeleted', index);
+      }
     }
   }
 </script>
